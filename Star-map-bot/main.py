65b30f1d6cddfde7522121440d33cba9f150df86
Kaito <tommychoiht@gmail.com>
2021-08-29 17:53:48 +0800

    Implemented star map basic functions

--- Star-map-bot/main.py
+++ Star-map-bot/main.py
@@ -1,12 +1,17 @@
+# https://www.epochconverter.com/
 
-import logging, os, asyncio # , requests
+# TODO custom location, pdf to image, red scale img, auto detect timezone of users, star map features toggles, etc.
+import logging
+import os
+import time
 
-from telegram import Update
+
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Updater, CommandHandler, CallbackContext
-from pyppeteer import launch # https://github.com/pyppeteer/pyppeteer
+# from pyppeteer import launch # https://github.com/pyppeteer/pyppeteer
 # from bs4 import BeautifulSoup # https://www.crummy.com/software/BeautifulSoup/bs4/doc/
-from datetime import *
-from pathlib import Path
+# from datetime import *
+# from pathlib import Path
 from dotenv import load_dotenv
 
 # Enable logging
@@ -19,31 +24,27 @@ logger = logging.getLogger(__name__)
 # Load credentials from enviroment variables
 load_dotenv()
 BOT_TOKEN = os.getenv('BOT_TOKEN')
-
-# Coordinates of Singapore 1.3521° N, 103.8198° E
-
-# async def get_image():
-#     ahora = datetime.now(timezone(timedelta(hours = 8)))
-#     url = f'https://in-the-sky.org/skymap2.php?no_cookie=1&latitude=1.3521&longitude=103.8198&timezone=+8.00&year={ahora.year}&month={ahora.month}&day={ahora.day}&hour={ahora.hour}&min={ahora.second}'
-#     browser = await launch()
-#     page = await browser.newPage()
-#     await page.goto(url)
-#     await page.screenshot(
-#         path = 'map.png',
-#         clip = dict(
-#             x = 0,
-#             y = 600,
-#             width = 800,
-#             height = 715
-#         ))
-#     await browser.close()
-
-
-# asyncio.get_event_loop().run_until_complete(get_image())
+SG_LAT = 1.3521 # Coordinates of Singapore 1.3521° N, 103.8198° E
+SG_LONG = 103.8198
+STAR_MAP_URL = f'https://www.heavens-above.com/SkyAndTelescope/StSkyChartPDF.ashx?time=%t&latitude={SG_LAT}&longitude={SG_LONG}&location=Singapore&utcOffset=28800000&showEquator=false&showEcliptic=true&showStarNames=true&showPlanetNames=true&showConsNames=true&showConsLines=true&showConsBoundaries=false&showSpecials=false&use24hClock=true'
 
 def start(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text('Stargazing bot')
 
-    update.message.reply_photo(photo = open('map.png', 'rb'))
+def send_star_map(update: Update, context: CallbackContext) -> None:
+    update.message.reply_document(
+        document = STAR_MAP_URL.replace('%t', str(int(time.time()*1000)))
+    )
+    update.message.reply_text('Enjoy the stunning stars! Be considerate and leave no trace while stargazing!')
+
+def credits(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text(
+        text = 'Star map is made available to you by skyandtelescope.org. This bot is not affiliated with skyandtelescope.org. Visit their website for more infomation.',
+        reply_markup = InlineKeyboardMarkup([
+            [InlineKeyboardButton("Visit skyandtelescope.org", url='https://skyandtelescope.org/')],
+            [InlineKeyboardButton("Check out their interactive sky chart", url='https://skyandtelescope.org/interactive-sky-chart/')]
+        ])
+    )
 
 def main() -> None:
     # Create the Updater and pass it your bot's token.
@@ -54,6 +55,8 @@ def main() -> None:
 
     # Register a command handler
     dispatcher.add_handler(CommandHandler('start', start))
+    dispatcher.add_handler(CommandHandler('starmap', send_star_map))
+    dispatcher.add_handler(CommandHandler('credits', credits))
 
     # Start the Bot using polling
     updater.start_polling()
